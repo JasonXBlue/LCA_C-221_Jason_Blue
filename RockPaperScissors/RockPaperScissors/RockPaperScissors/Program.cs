6d7bdb6e263e0b1512c2fd1a15175cd04526fada
JasonXBlue <JasonX.blue1@gmail.com>
2019-10-20 19:05:42 -0500

    10/20/19 7:05pm

--- RockPaperScissors/RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors/Program.cs
@@ -6,6 +6,20 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            Game();
+            Console.WriteLine("Play again? (yes/no)");
+            while (Console.ReadLine().ToLower() == "yes")
+            {
+                Game();
+            }
+            
+            {
+                Console.WriteLine("Goodbye");
+            }
+               
+        }
+
+        /*{
             Console.WriteLine("Lets go! Rock, Paper, Scissors?");
             string hand1 = Console.ReadLine().ToLower();
 
@@ -30,9 +44,40 @@ namespace RockPaperScissors
                     Console.WriteLine("scissors");
                     hand2 = "scissors";
                     break;
-            }
+            }*/
 
+
+        //CompareHands(hand1, hand2);
+
+        //}
+        public static void Game()
+        {
+            Console.WriteLine("Lets go! Rock, Paper, Scissors?");
+            string hand1 = Console.ReadLine().ToLower();
+
+            Random generator = new Random();
+            // creates a number 0,1 or 2
+            int randomNumber = generator.Next(0, 3);
+            string hand2 = null;
             
+
+            //assigns computers play of rock, paper or scissors randomly
+
+            switch (randomNumber)
+            {
+                case 0:
+                    Console.WriteLine("rock");
+                    hand2 = "rock";
+                    break;
+                case 1:
+                    Console.WriteLine("paper");
+                    hand2 = "paper";
+                    break;
+                case 2:
+                    Console.WriteLine("scissors");
+                    hand2 = "scissors";
+                    break;
+            }
             CompareHands(hand1, hand2);
 
         }
@@ -40,19 +85,23 @@ namespace RockPaperScissors
 
             public static void CompareHands(String hand1, String hand2)
         {
-            
+           
+
             if ((hand1 == "rock") && (hand2 == "scissors"))
             {
                 Console.WriteLine("You win!");
+                
             }
 
             else if ((hand1 == "scissors") && (hand2 == "paper"))
             {
                 Console.WriteLine("You win!");
+                
             }
             else if ((hand1 == "paper") && (hand2 == "rock"))
             {
                 Console.WriteLine("You win!");
+                
             }
             else if (hand1 == hand2)
             {
@@ -61,6 +110,7 @@ namespace RockPaperScissors
             else
             {
                 Console.WriteLine("Computer wins!");
+                
             }
             
 
