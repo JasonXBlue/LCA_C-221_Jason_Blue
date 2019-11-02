fce8d950799f00f29f775b6f0d3a5f627d004cb5
JasonXBlue <JasonX.blue1@gmail.com>
2019-11-02 10:44:40 -0500

    11/2/19 10:44am

--- Gradebook/Gradebook/Program.cs
+++ Gradebook/Gradebook/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Globalization;
+using System.Linq; //for use of average, .Max & .Min
+using System.Globalization; //for Culture.Info to capitalize name correctly
 
 namespace Gradebook
 {
     class Program
     {
-        public static string Input;
-        public static string Name;
-        public static string Grades;
+        public static string Input;  //variable to determine if quit entered to stop input
+        public static string Name;   //dictionary variable to store unique name
+        public static string Grades; //dictionary variable to store grades inputed for each name
         public static Dictionary<string, string> StudentGrades = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            while (true)
+            while (true) //loop that ask input for students until quit is entered
             {
                 Console.WriteLine("Enter a student's name or 'quit' to end: ");
                 Input = Console.ReadLine().ToLower();
                 if (Input != "quit")
                 {
                     Name = Input;
-                    //Name = char.ToUpper(Name[0]) + Name.Substring(1);
-                    Name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Name.ToLower());
+                    Name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Name.ToLower()); //capitalizes first letter of first and last name
                 }       
                 else if (Input == "quit")
                 {
@@ -29,19 +28,19 @@ namespace Gradebook
                 }
                 Console.WriteLine("Enter the students grades separated by spaces (100 90 78 101 45 81) :");
                 Grades = Console.ReadLine();
-                StudentGrades.Add(Name, Grades);
+                StudentGrades.Add(Name, Grades); //adds name and grades to dictionary
             }
             Calculations();
         }
-        static void Calculations()
+        static void Calculations() //calculates and displays average, min and max grades for each unique name 
         {
             foreach (var item in StudentGrades)
             {   
-                string[] Nums = item.Value.Split(" ", item.Value.Length, StringSplitOptions.RemoveEmptyEntries);
-                int[] myInts = Array.ConvertAll(Nums, int.Parse);
+                string[] Nums = item.Value.Split(" ", item.Value.Length, StringSplitOptions.RemoveEmptyEntries); //string array with spaces removed for grades entered
+                int[] myInts = Array.ConvertAll(Nums, int.Parse); //convert string array to int array for grade calculations
                 decimal average = (decimal)myInts.Average();
                 Console.WriteLine(" ");
-                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Key); //prints each unique name
                 Console.WriteLine("\n" + "Average grade is : " + average);
                 Console.WriteLine("Highest grade is : " + myInts.Max());
                 Console.WriteLine("Lowest  grade is : " + myInts.Min());
