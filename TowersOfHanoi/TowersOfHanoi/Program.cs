189cc25e9eb81fbafb565bc088b67f9b111eb55f
JasonXBlue <JasonX.blue1@gmail.com>
2019-11-03 12:05:15 -0600

    11/3/19 12:05pm

--- TowersOfHanoi/TowersOfHanoi/Program.cs
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -6,8 +6,6 @@ namespace TowersOfHanoi
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Towers of Hanoi!" + "\n");
@@ -15,7 +13,7 @@ namespace TowersOfHanoi
             Console.WriteLine("1. Blocks can only be moved to be on top of larger blocks.");
             Console.WriteLine("2. Only the top block from any stack can be moved.");
             Console.WriteLine("3. You win the game by moving entire stack from tower A to Tower C." + "\n");
-            Dictionary<string, Stack<int>> towers = new Dictionary<string, Stack<int>>()
+            Dictionary<string, Stack<int>> Towers = new Dictionary<string, Stack<int>>()
             {
                 {"A", new Stack<int>() },
                 {"B", new Stack<int>() },
@@ -23,51 +21,43 @@ namespace TowersOfHanoi
             };
             for (int i = 1; i <= 4; i++)
             {
-                towers["A"].Push(i);
-                //towers["B"].Push(0);
-                //towers["C"].Push(0);
+                Towers["A"].Push(i);
             }
-            GameBoard(towers);
+            GameBoard(Towers);
             Console.WriteLine("\n" + "Which tower do you want to move the top piece from?");
-            string Piece = Console.ReadLine().ToUpper();
+            string FromStack = Console.ReadLine().ToUpper();
             Console.WriteLine("\n" + "Which tower do you want to move the piece to?");
-            string Place = Console.ReadLine().ToUpper(); 
+            string ToStack = Console.ReadLine().ToUpper();
+            LegalMove(Towers, FromStack, ToStack);
         }
-        static void GameBoard(Dictionary<string, Stack<int>> towers)
+        static void GameBoard(Dictionary<string, Stack<int>> Towers)
         {
-            foreach (var item in towers)
+            foreach (var item in Towers)
             {
 
 
                 Console.Write(item.Key + "| ");
                 foreach (int X in item.Value)
                 {
-                    if (X == 0)
-                        Console.Write(" - ");
-                    else
-                        Console.Write(" " + X + " ");
+                    Console.Write(" " + X + " ");
                 }
                 Console.Write("\n");
-
-
             }
 
         }
-        //static void Test(Dictionary<string, Stack<int>> towers, string Piece, string Place)
-        //{
-        //    foreach (var item in towers)
-        //    {
-        //        foreach (char Y in item.Key)
-        //        {
-        //            if (Y = Piece)
-        //            {
-
-        //            }
-        //        }
-        //    }
-           
-        //}
-       
+        public static void LegalMove(Dictionary<string, Stack<int>> Towers, string FromStack, string ToStack)
+        {
+            
+            
+            if (Towers[FromStack].Count == 0)
+            {
+                Console.WriteLine("Illegal move. Please enter a valid tower: ");
+            }
+            else if (Towers[FromStack].Peek() > Towers[ToStack].Peek())
+            {
+                Console.WriteLine("Illegal move. Please enter a valid tower: ");
+            }
+        }
 
 
     }
