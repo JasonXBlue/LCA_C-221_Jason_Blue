3e8c221020d652fe61d3bb60f8d28235313856bc
JasonXBlue <JasonX.blue1@gmail.com>
2019-11-06 20:30:36 -0600

    11/6/19 8:30pm

---
+++ TodoItems/TodoItems/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TodoItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Check = false;
+            bool Quit = false;
+            while (!Check)
+            {
+                Console.WriteLine("Enter A to add an item or Enter Q to quit:");
+                String Response = Console.ReadLine().ToUpper();
+                if (Response == "A")
+                {
+                    Check = true;
+                }
+                else if (Response == "Q")
+                {
+                    Check = true;
+                    Quit = true;
+                }
+                else
+                {
+                    Check = false;
+                    Console.WriteLine("Invalid entry.Enter A to add an item or Enter Q to quit:");  
+                }
+            }
+            while (!Quit)
+            {
+                Console.WriteLine("Please enter an item");
+            }
+            
+        }
+    }
+    class ToDoItem
+    {
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public string Priority { get; set; }
+    }
+
+
+}
+
