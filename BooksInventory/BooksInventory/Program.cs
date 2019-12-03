37984774a7865cc160d7bb4fa8c89062a198bed6
JasonXBlue <JasonX.blue1@gmail.com>
2019-12-02 19:59:29 -0600

    12/2/19

--- BooksInventory/BooksInventory/Program.cs
+++ BooksInventory/BooksInventory/Program.cs
@@ -17,7 +17,6 @@ namespace BooksInventory
     }
     class BooksContext : DbContext
     {
-
         // This property corresponds to the table in our database
         public DbSet<Book> Books { get; set; }
 
@@ -40,52 +39,46 @@ namespace BooksInventory
             bool Quit = false;
             while (!Quit)
             {
-
-
-                // ask the user for a student to add
-                Console.WriteLine("Enter book title and author or Q to Quit:");
-                String fullBook = Console.ReadLine();
-                String[] parts = fullBook.Split();
-                if (fullBook == "Q")
+                // ask the user for a book to add
+                string[] Parts = new string[2];
+                Console.WriteLine("Enter book title or Q to Quit:");
+                String Title = Console.ReadLine();
+                if (Title.ToUpper() == "Q")
                 {
                     Quit = true;
                 }
-
-                // split the input into parts, and make sure 
-                // we have 2 parts only
-                //String[] parts = fullBook.Split();
-
-                else if (parts.Length == 2)
-
-                {
-                    // create a new student object, notice that we do not 
-                    // select an id, we let the framework handle that
-                    Book newBook = new Book(parts[0], parts[1]);
-
-                    // add the newly created student instance to the context
-                    // notice how similar this is to adding a item to a list,
-                    context.Books.Add(newBook);
-
-                    // ask the context to save any changes to the database 
-                    context.SaveChanges();
-                    Console.WriteLine("Added the book.");
-                }
                 else
                 {
-                    Console.WriteLine("Invalid book title and author, did not add book");
-                }
+                    Parts[0] = Title;
+                    Console.WriteLine("Enter the book author:");
+                    String Author = Console.ReadLine();
+                    Parts[1] = Author;
+                    {
+                        // create a new book object, notice that we do not 
+                        // select an id, we let the framework handle that
+                        Book newBook = new Book(Parts[0], Parts[1]);
 
-                Console.WriteLine("The Current List of books are: ");
-                // use a for each loop to loop through the students in the context
-                // notice how similar this is to looping through a list
-                foreach (Book s in context.Books)
-                {
-                    Console.WriteLine("{0} - {1} {2}",
-                         s.Id, s.Title, s.Author);
-                }
+                        // add the newly created book instance to the context
+                        // notice how similar this is to adding a item to a list,
+                        context.Books.Add(newBook);
 
+                        // ask the context to save any changes to the database 
+                        context.SaveChanges();
+                        Console.WriteLine("Added the book.");
+                    }
+                }
+                    Console.WriteLine("The Current List of books are: \n");
+                    // use a for each loop to loop through the books in the context
+                    // notice how similar this is to looping through a list
+                    foreach (Book s in context.Books)
+                    {
+                        Console.WriteLine("{0} - Title: {1} | Author: {2}",
+                             s.Id, s.Title, s.Author);
+                    }
+
+                }
             }
         }
     }
-    }
+
 
